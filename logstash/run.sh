bbf3d9e8eb784466067f59f956ecf504a78e1e56
Felix Massem <felix.massem@codecentric.de>
2014-12-22 15:13:50 +0100

    changed elasticsearch version to 1.3.4 & clean up

--- logstash/run.sh
+++ logstash/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# #################################################################
-# NAME: kibana.sh
-# DESC: Kibana/nginx startup file.
-# #################################################################
 
 exec /opt/logstash/bin/logstash agent --debug --verbose -f /etc/logstash/conf.d/logstash.conf
 exec "$@"
